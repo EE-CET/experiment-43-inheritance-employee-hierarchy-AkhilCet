8c97c76aabba3609f0ac38441c8ff63fe8e34c7c
AkhilCet <tve24el005@cet.ac.in>
2026-02-11 12:54:10 +0530

    Update EmployeeInheritance.java

--- src/EmployeeInheritance.java
+++ src/EmployeeInheritance.java
@@ -1,33 +1,81 @@
 import java.util.Scanner;
 
 class Employee {
-   
+   String name;
+   int age;
+   double phoneNumber;
+   string address;
+   float salary;
+
+   Employee(String n,int a,double pN,string ad,float s)
+   {
+      this.name = n;
+      this.age = a;
+      this.phoneNumber = pN;
+      this.address = ad;
+      this.salary = s;
+   }
 }
 
-// TODO: Create class Officer extends Employee
-// Add 'specialization' attribute
+class Officer extends Employee{
+   string specialization;
 
-// TODO: Create class Manager extends Employee
-// Add 'department' attribute
+   Officer(String n,int a,double pN,string ad,float s,String sp)
+   {
+      super(n,a,pN,ad,s);
+      this.specialization = sp;
+   }
+}
+
+class Manager extends Employee{
+   string department;
+
+   Officer(String n,int a,double pN,string ad,float s,String d)
+   {
+      super(n,a,pN,ad,s);
+      this.department = d;
+   }
+}
+
 
 public class EmployeeInheritance {
-   
-        
-        // --- OFFICER INPUT ---
-        // Create Officer object
-        // Read name, age, phone, address, salary, specialization
-        // Hint: Use sc.nextLine() for strings to capture spaces, sc.nextInt() for numbers.
-        // Be careful with buffer clearing after nextInt()!
-        
-        // --- MANAGER INPUT ---
-        // Create Manager object
-        // Read name, age, phone, address, salary, department
-        
-        // --- OUTPUT ---
-        // Print "Officer:"
-        // Print officer details (one per line)
-        
-        // Print "Manager:"
-        // Print manager details (one per line)
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      String name = input.nextLine();
+      int age = input.nextInt();
+      double phone = input.nextDouble();
+      String ad = input.nextLine();
+      float sal = input.nextFloat();
+      String spec = input.nextLine();
+
+      Officer off1 = new Officer(name,age,phone,ad,sal,spec);
+
+      name = input.nextLine();
+      age = input.nextInt();
+      phone = input.nextDouble();
+      ad = input.nextLine();
+      sal = input.nextFloat();
+      spec = input.nextLine();
+      
+      Manager man1 = new Manager(name,age,phone,ad,sal,spec);
+
+      Syetem.out.println("Offcier:")
+      Syetem.out.println(off1.name)
+      Syetem.out.println(off1.age)
+      Syetem.out.println(off1.phoneNumber)
+      Syetem.out.println(off1.address)
+      Syetem.out.println(off1.salary)
+      Syetem.out.println(off1.specialization)
+
+      Syetem.out.println("Manager:")
+      Syetem.out.println(man1.name)
+      Syetem.out.println(man1.age)
+      Syetem.out.println(man1.phoneNumber)
+      Syetem.out.println(man1.address)
+      Syetem.out.println(man1.salary)
+      Syetem.out.print(man1.department)
+      
+   }
     
 }
